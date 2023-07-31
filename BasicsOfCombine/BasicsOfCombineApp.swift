5b8836e8aaa745ea43c46d892ad92700451ed90e
Virendra Gupta <>
2023-07-31 15:08:51 +0530

    first commit

---
+++ BasicsOfCombine/BasicsOfCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicsOfCombineApp.swift
+//  BasicsOfCombine
+//
+//  Created by Virendra Gupta on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicsOfCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
